35ba67943d781fc603cd609e6b05407b5877b0ea
Paulo Andrade <paulo@outercorner.com>
2019-02-14 13:51:58 +0000

    Initial Commit

---
+++ LateResponders/LateResponders.h
@@ -0,0 +1,19 @@
+//
+//  LateResponders.h
+//  LateResponders
+//
+// Created by Paulo Andrade on 14/02/2019.
+// Copyright © 2019 Outer Corner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LateResponders.
+FOUNDATION_EXPORT double LateRespondersVersionNumber;
+
+//! Project version string for LateResponders.
+FOUNDATION_EXPORT const unsigned char LateRespondersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LateResponders/PublicHeader.h>
+
+
